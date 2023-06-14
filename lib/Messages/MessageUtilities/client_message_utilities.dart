81b958e1b7a82804e2a7b55a94722caad126724b
Mark Victor Kithinji <105651925+MarkKithinji@users.noreply.github.com>
2023-06-14 11:18:34 +0300

    Add files via upload
    Created new utilities for the messages section as follows:
    1. new text bubble utilities

--- lib/Messages/MessageUtilities/client_message_utilities.dart
+++ lib/Messages/MessageUtilities/client_message_utilities.dart
@@ -1,71 +1,62 @@
 import 'package:flutter/material.dart';
 
-class ClientMessageBox extends StatefulWidget {
-  String clientName;
-  String clientMessage;
-  String timeStamp;
-  ClientMessageBox(
-      {required this.clientName,
-      required this.clientMessage,
-      required this.timeStamp});
 
-  @override
-  State<ClientMessageBox> createState() => _ClientMessageBoxState();
-}
-
-class _ClientMessageBoxState extends State<ClientMessageBox> {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Container(
-
-        )
-      ],
-    );
-  }
-}
-
-
-class MessageBubble extends StatelessWidget {
-  //String clientName;
-  //String clientMessage;
-  //String timeStamp;
+class TextBubble extends StatelessWidget {
   final String message;
-  final bool isMe;
-  final Key key;
+  final String sender;
+  final String time;
 
-  MessageBubble({required this.message, required this.isMe, required this.key});
+  TextBubble({required this.message, required this.sender, required this.time});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-      children: [
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          width: MediaQuery.of(context).size.width * 0.6,
-          decoration: BoxDecoration(
-            color: isMe ? Colors.blue[300] : Colors.grey[300],
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
-              bottomLeft: isMe ? Radius.circular(12) : Radius.zero,
-              bottomRight: isMe ? Radius.zero : Radius.circular(12),
+    final isUser = sender.toLowerCase() == "user";
+    final bubbleColor = isUser ? Colors.grey[300] : Colors.grey[600];
+    final outlineColor = isUser ? Colors.grey : Colors.white;
+    final textColor = isUser ? Colors.grey[900] : Colors.grey[300];
+
+    return Container(
+      margin: EdgeInsets.only(
+        left: isUser ? 48.0 : 8.0,
+        right: isUser ? 8.0 : 48.0,
+      ),
+      padding: EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: bubbleColor,
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(color: outlineColor),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          child: Text(
+          SizedBox(height: 4.0),
+          Text(
             message,
             style: TextStyle(
-              color: isMe ? Colors.white : Colors.black,
+              color: textColor,
             ),
           ),
-        ),
-      ],
+          SizedBox(height: 8.0),
+          Text(
+            time,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 12.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
+
+
 
